3e6ca386b3c15e9f94b25a8368c084edf3c70c83
Milind <milind.official98@gmail.com>
2024-09-29 10:25:35 +0200

    banner images aligned in one

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ const banner = document.getElementById('banner');
 let lastScrollTop = 0;
 
 window.addEventListener('scroll', function() {
-const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-if (scrollTop > lastScrollTop) {
-    // Scrolling down
-    banner.classList.add('shrink'); // Add class to shrink the banner
-    banner.style.overflow = 'hidden'; // Hide overflow
-} else {
-    // Scrolling up
-    banner.classList.remove('shrink'); // Remove class to expand the banner
-    banner.style.overflow = 'visible'; // Show overflow
-}
-lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
-
+    if (scrollTop > lastScrollTop) {
+        // Scrolling down
+        banner.classList.add('shrink'); // Add class to shrink the banner
+        banner.style.overflow = 'hidden'; // Hide overflow
+    } else {
+        // Scrolling up
+        banner.classList.remove('shrink'); // Remove class to expand the banner
+        banner.style.overflow = 'visible'; // Show overflow
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
 });
